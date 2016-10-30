b8796e4aa592bffd449b0f0bebcdcbb38e207ada
Victor Hazali <victorhazali@gmail.com>
2016-10-30 19:03:15 +0800

    Added first page

--- Frontend/index.php
+++ Frontend/index.php
@@ -4,9 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
+    <title>Learn Chinese</title>
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Favicon -->
@@ -20,7 +18,19 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+    <!-- Welcome message -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="text-center"> Hello! Welcome to Learn Chinese!</h1>
+      </div>
+    </div>
+
+    <div class="center-block">
+        <button id="startbutton" name="startbutton" class="btn btn-primary btn-lg center-block">
+            Click on me to Start!
+        </button>
+    </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
