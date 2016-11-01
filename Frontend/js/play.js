c10620d623f52ba72478e048211910e4e8b443ab
Victor Hazali <victorhazali@gmail.com>
2016-11-01 17:01:28 +0800

    Added colour coding for wrong and correct choices

--- Frontend/js/play.js
+++ Frontend/js/play.js
@@ -4,19 +4,26 @@ $(function(){
     .ajaxStart(function () {
         loading.show();
     }).ajaxStop(function () {
-    	loading.hide();
+      loading.hide();
     });
     
     $("label.btn").on('click',function () {
-    	var choice = $(this).find('input:radio').val();
-    	$('#loadbar').show();
-    	$('#quiz').fadeOut();
-    	setTimeout(function(){
-           $( "#answer" ).html(  $(this).checking(choice) );      
+      var choice = $(this).find('input:radio').val();
+      $('#loadbar').show();
+      $('#quiz').fadeOut();
+      setTimeout(function(){
+          var answer = $(this).checking(choice);
+           $( "#answer" ).html(answer);
             $('#quiz').show();
+            // Display the correct choice in green
+            document.getElementById("option-"+$ans).className=document.getElementById("option-"+$ans).className.replace( /(?:^|\s)btn-primary(?!\S)/g , ' btn-success' );
+            // Display the wrong answer in red
+            if(answer == 'INCORRECT') {
+              document.getElementById("option-"+choice).className=document.getElementById("option-"+choice).className.replace( /(?:^|\s)btn-primary(?!\S)/g , ' btn-danger' );
+            }
             $('#loadbar').fadeOut();
            /* something else */
-    	}, 1500);
+      }, 1500);
     });
 
     $ans = 3;
@@ -27,4 +34,4 @@ $(function(){
         else 
             return 'CORRECT';
     }; 
-});	
+}); 
--- Frontend/pages/play.php
+++ Frontend/pages/play.php
@@ -44,10 +44,10 @@
             </div>
 
             <div class="quiz" id="quiz" data-toggle="buttons">
-              <label class="element-animation1 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="1">1 One</label>
-              <label class="element-animation2 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="2">2 Two</label>
-              <label class="element-animation3 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="3">3 Three</label>
-              <label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="radio" name="q_answer" value="4">4 Four</label>
+              <label class="element-animation1 btn btn-lg btn-primary btn-block " id="option-1"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right" id="option-1-icon"></i></span> <input type="radio" name="q_answer" value="1">1 One</label>
+              <label class="element-animation2 btn btn-lg btn-primary btn-block" id="option-2"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right" id="option-2-icon"></i></span> <input type="radio" name="q_answer" value="2">2 Two</label>
+              <label class="element-animation3 btn btn-lg btn-primary btn-block" id="option-3"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right" id="option-3-icon"></i></span> <input type="radio" name="q_answer" value="3">3 Three</label>
+              <label class="element-animation4 btn btn-lg btn-primary btn-block" id="option-4"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right" id="option-4-icon"></i></span> <input type="radio" name="q_answer" value="4">4 Four</label>
             </div>
           </div>
           <div class="modal-footer">
