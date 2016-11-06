1218386503dcd9a0ef2676d31bbcbbfad509d23c
Victor Hazali <victorhazali@gmail.com>
2016-11-06 13:11:43 +0800

    Now use local jQuery instead

--- Frontend/index.php
+++ Frontend/index.php
@@ -36,7 +36,7 @@
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="Frontend/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="Frontend/bootstrap/js/bootstrap.min.js"></script>
   </body>
